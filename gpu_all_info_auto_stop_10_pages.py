b42c84771bc3629c495a1f57e107a8445b173333
drrci <gjentig4@gmail.com>
2023-10-01 15:49:55 +0300

    gpu all info fetcher + gpu all info data cleaning

--- gpu_all_info_auto_stop_10_pages.py
+++ gpu_all_info_auto_stop_10_pages.py
@@ -3,34 +3,25 @@ from bs4 import BeautifulSoup
 import csv
 import os
 import time
-import schedule
 
-base_url = 'https://www.tori.fi/uusimaa/tietokoneet_ja_lisalaitteet/komponentit?ca=18&cg=5030&c=5038&ps=1&st=s&st=k&st=u&st=h&st=g&com=graphic_card&w=1&o=1'
+base_url = 'https://www.tori.fi/uusimaa/tietokoneet_ja_lisalaitteet/komponentit?ca=18&cg=5030&c=5038&ps=1&st=s&st=k&st=u&st=h&st=g&com=graphic_card&w=1&o='
 filename = '123.csv'
-processed_links_file = 'processed_links.txt'  # Define the processed_links_file
+processed_links_file = 'processed_links.txt'
 
-def get_item_details(link):    
+
+def get_item_details(link):
     print(f"Fetching details from {link}")
     response = requests.get(link)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Finding the title
         title_div = soup.find('div', class_='topic')
         title = title_div.find('h1').text.strip() if title_div else 'Title not found'
-        
-        # Finding the price
         price = soup.find(itemprop='price')
         price = price['content'] if price and price.has_attr('content') else 'Price not found'
-        
-        # Finding the description
         description_div = soup.find('div', class_='body', itemprop='description')
         description = description_div.text.strip() if description_div else 'Description not found'
-        
         print(f"Fetched details: Title - {title}, Price - {price}")
-        
         return {'Title': title, 'Price': price, 'Description': description, 'Link': link}
-        
     else:
         print(f"Failed to retrieve details from {link}: {response.status_code}")
         return {'Title': 'Failed to retrieve', 'Price': 'Failed to retrieve', 'Description': 'Failed to retrieve', 'Link': link}
@@ -43,51 +34,65 @@ def scrape_page(url, processed_links):
         soup = BeautifulSoup(response.text, 'html.parser')
         links = [a['href'] for a in soup.select('div.list_mode_thumb a[href]')]
         print(f"Found {len(links)} links")
-        
         items_details = []
         for link in links:
             full_link = 'https://www.tori.fi' + link if not link.startswith('http') else link
             if full_link in processed_links:
                 print(f"Skipping already processed link: {full_link}")
-                continue  # Skip the link if it's already processed.
-                
-            item_details = get_item_details(full_link)  # Fetch details only for unprocessed link.
+                continue
+            item_details = get_item_details(full_link)
             items_details.append(item_details)
-            processed_links.add(full_link)  # Add the link to processed_links set after processing.
+            processed_links.add(full_link)
             time.sleep(0.5)
-            
         return items_details
     else:
         print(f"Failed to retrieve page {url}: {response.status_code}")
         return []
 
 
-# Load processed links
 processed_links = set()
 if os.path.exists(processed_links_file):
     with open(processed_links_file, 'r') as f:
         processed_links = set(line.strip() for line in f)
-
-item_details_list = scrape_page(base_url, processed_links)  # Pass the processed_links set to the scrape_page function.
 
 file_exists = os.path.isfile(filename)
 
 with open(filename, 'a', newline='', encoding='utf-8') as file:
     fieldnames = ['Title', 'Price', 'Description', 'Link']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
-    
+
     if not file_exists:
         writer.writeheader()
-    
-    for item_details in item_details_list:
-        link = item_details.get('Link')
-        writer.writerow(item_details)
-        processed_links.add(link)
 
-# Save processed links
-with open(processed_links_file, 'w') as f:
-    for link in processed_links:
-        f.write(f"{link}\n")
+    # Your other code above ...
+
+# Iterate over the desired range of pages
+for page_number in range(2, 5):  # Or any other range you desire
+    url = base_url + str(page_number)
+    print(f"Scraping page {page_number}")
+        
+    # Call the function to scrape the page
+    items_details_list = scrape_page(url, processed_links)
+    
+    if not items_details_list:  # Exit the loop if no links are found on the page
+        print("No links found on page {page_number}. Exiting.")
+        break
+    
+    with open(filename, 'a', newline='', encoding='utf-8') as file:
+        fieldnames = ['Title', 'Price', 'Description', 'Link']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        
+        if not file_exists:
+            writer.writeheader()
+        
+        for item_details in items_details_list:
+            link = item_details.get('Link')
+            writer.writerow(item_details)
+            processed_links.add(link)
+        
+    # Save processed links
+    with open(processed_links_file, 'w', encoding='utf-8') as f:
+        for link in processed_links:
+            f.write(f"{link}\n")
 
 print("Scraping Completed.")
-
---
+++ gpu_all_info_data_cleaning.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import re
+
+# Define a list of keywords that you will search for in the title and description.
+gpu_keywords = [ '960','970','980','1050','1060', '1070', '1080', '2060', '2070', '2080', '3060', '3070', '3080', '3090', '4090',
+                 '470','480', '390', '580 ','570 ', '590', '6700', '6800', '6900', '5500' ,'5700', '5600', 'Vega',
+                '4070', '3090', '7900', '4080', '6950', '7800',
+                '6750', '4060', '6600', '6650', '7600',
+                '6500', '3050', '6400', '1660', '1650', 'titan', 'quadro']
+
+# Load the CSV file into a pandas DataFrame
+filename = '123.csv'
+data = pd.read_csv(filename)
+
+# Initialize new columns for the GPU category and VRAM
+data['GPU_Category'] = 'Unknown'
+data['VRAM'] = 'Unknown'
+
+# Define a function to categorize GPUs and find VRAM based on keywords in title and description
+def categorize_gpu_and_find_vram(row):
+    # Combine title and description and convert to lower case
+    combined_text = (str(row['Title']) + ' ' + str(row['Description'])).lower()
+    
+    # Categorize GPU
+    for keyword in gpu_keywords:
+        if keyword.lower() in combined_text:
+            row['GPU_Category'] = keyword
+            break  # Stop the loop once a keyword is found
+    
+    # Find VRAM with case insensitive search
+    vram_match = re.search(r'(?i)(\d+\s?GB)', combined_text)
+    if vram_match:
+        row['VRAM'] = vram_match.group(1).strip()
+        
+    return row
+
+# Apply the function to each row in the DataFrame
+data = data.apply(categorize_gpu_and_find_vram, axis=1)
+
+# Reorder the columns list to place 'GPU_Category' as the 3rd column and 'VRAM' as the 4th column
+columns_list = data.columns.tolist()
+new_columns_list = columns_list[:2] + ['GPU_Category', 'VRAM'] + columns_list[2:-2]
+data = data[new_columns_list]
+
+# Save the categorized data and VRAM to a new CSV file with the new column order
+data.to_csv('gpu_cleaned_data.csv', index=False)
